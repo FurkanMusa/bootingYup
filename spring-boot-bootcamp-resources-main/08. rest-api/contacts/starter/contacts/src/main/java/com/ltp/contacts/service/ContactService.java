57a51ce79cc77bf9b45d548c68145bdfbaa40a13
furk <helloitsmefurki@gmail.com>
2024-07-04 00:38:24 +0300

    end of lesson 161

--- spring-boot-bootcamp-resources-main/08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactService.java
+++ spring-boot-bootcamp-resources-main/08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactService.java
@@ -1,5 +1,13 @@
 package com.ltp.contacts.service;
 
+import org.springframework.stereotype.Service;
+
+import com.ltp.contacts.pojo.Contact;
+
 public interface ContactService {
     
+    Contact getContactById(String id);
+
+    void saveContact(Contact contact);
+
 }
